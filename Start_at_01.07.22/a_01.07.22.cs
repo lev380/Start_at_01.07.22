927f30d8a62ec24077109ce0e0abd6d7d4a14a22
Lev <lev380@mail.ru>
2022-07-01 23:17:00 +0300

    Add project files.

---
+++ Start_at_01.07.22/a_01.07.22.cs
@@ -0,0 +1,41 @@
+﻿using System;
+// This namespace to use some commands
+/*
+ * зеленый текст игнорируется программой
+*/
+// Testing program, trashbox))
+ 
+
+namespace Start_at_01._07._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string name=Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " !!!!");
+            */
+
+            /*  Console.WriteLine("Input first value");
+
+              int value = int.Parse(Console.ReadLine());
+
+              Console.WriteLine("Input first value");
+
+              int value2 = int.Parse(Console.ReadLine());
+
+              Console.WriteLine("Сумма чисел равна =  " + value+value2);
+            */
+
+            string f = "5,4";
+
+            double a = Convert.ToDouble(f);
+
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
